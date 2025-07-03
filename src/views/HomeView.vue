1b75e81077c8839c564e2a0a0b13e3e724e2e82d
Melani <mlazoa@ulasalle.edu.pe>
2025-07-02 20:33:17 -0500

    Actualizando informacion

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -19,7 +19,7 @@
           <iframe
             width="100%"
             height="400"
-            src="https://www.youtube.com/embed/hlDabCyVRQQ"
+            src="https://www.youtube.com/embed/vbbPMHNGfkk"
             title="Video de bienvenida"
             frameborder="0"
             allowfullscreen
